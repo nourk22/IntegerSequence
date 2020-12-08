f8b89d618b15abcbb59648f2d87f66319e5b1e50
Nour <>
2020-12-08 12:57:46 -0500

    compiles, but constructor does not work

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,7 +4,12 @@ public class ArraySequence implements IntegerSequence{
   private int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+    data = new int[other.length];
+    for (int i=0; i<other.length; i++){
+      data[i]=other[i];
+    }
+  }
 
 
   public boolean hasNext(){
@@ -20,10 +25,16 @@ public class ArraySequence implements IntegerSequence{
   }
 
   public void reset(){
-    
+
   }
 
   public static void main (String[] args){
+    int[]nums = {1,3,5,0,-1,3,9};
+    IntegerSequence as = new ArraySequence(nums);
 
+    System.out.println("ArraySequence(array):");
+    while(as.hasNext()){
+      System.out.print(as.next()+", ");
+    }
   }
 }
